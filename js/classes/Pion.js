3b7ccceb08563cb3141191b7f72d1407d70f12eb
Joao-prrrr <joao.prrrr@eduge.ch>
2022-09-26 09:34:19 +0200

    Classe pion créée

---
+++ js/classes/Pion.js
@@ -0,0 +1,7 @@
+let Pion = function(data) {
+    this.data = data;
+    // id: document.getElementById('pionBleu'),
+    // enJeux: false,
+    // case: 0,
+    // position: {top: '70%', left: '0%'}
+}
