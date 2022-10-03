5ecc383a4c3e947f93930e11fc232ae28732db8b
Louis-Saulnier <louis.slnr@eduge.ch>
2022-10-03 09:24:49 +0200

    menu.js finish

--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,36 @@
-const deuxJchck     = document.getElementById("deuxJ");
-const troisJchck    = document.getElementById("troisJ");
-const quatreJchck   = document.getElementById("quatreJ");
-const troisJinput   = document.getElementById("j3");
-const quatreJinput  = document.getElementById("j4");
-const jUnName       = document.getElementById("joueurUn").value;
-const jDeuxName     = document.getElementById("joueurDeux").Value;
-const jTroisName    = document.getElementById("joueurTrois").value;
-const jQuatreName   = document.getElementById("joueurQuatre").value;
-const rougeSelect   = true;
-const bleuSelect    = true;
-let   vertSelect    = true;
-let   orangeSelect  = true;
+const deuxJchck = document.getElementById("deuxJ");
+const troisJchck = document.getElementById("troisJ");
+const quatreJchck = document.getElementById("quatreJ");
+const troisJinput = document.getElementById("j3");
+const quatreJinput = document.getElementById("j4");
+const jUn = document.getElementById("joueurUn");
+const jDeux = document.getElementById("joueurDeux");
+const jTrois = document.getElementById("joueurTrois");
+const jQuatre = document.getElementById("joueurQuatre");
+const rougeSelect = true;
+const bleuSelect = true;
+let vertSelect = true;
+let orangeSelect = true;
 
 let listJoueur = [];
 
 
 
-deuxJchck.addEventListener("click", function(){
+deuxJchck.addEventListener("click", function() {
     troisJinput.style.display = "none";
     vertSelect = false;
     quatreJinput.style.display = "none";
     orangeSelect = false;
 });
 
-troisJchck.addEventListener("click", function(){
+troisJchck.addEventListener("click", function() {
     troisJinput.style.display = "flex";
     vertSelect = true;
     quatreJinput.style.display = "none";
     orangeSelect = false;
 })
 
-quatreJchck.addEventListener("click", function(){
+quatreJchck.addEventListener("click", function() {
     troisJinput.style.display = "flex";
     vertSelect = true;
     quatreJinput.style.display = "flex";
@@ -38,13 +38,25 @@ quatreJchck.addEventListener("click", function(){
 });
 
 btnStart.addEventListener('click', function() {
+    listJoueur = [];
+    const jUnName = jUn.value;
+    const jDeuxName = jDeux.value;
+    const jTroisName = jTrois.value;
+    const jQuatreName = jQuatre.value;
     listJoueur.push(jUnName);
     listJoueur.push(jDeuxName);
-    if(vertSelect === true)
-    {
+    console.log(listJoueur[0]);
+    console.log(listJoueur[1]);
+    if (vertSelect === true) {
         listJoueur.push(jTroisName);
-        if(orangeSelect === true){
+        console.log(listJoueur[2]);
+        if (orangeSelect === true) {
             listJoueur.push(jQuatreName);
+            console.log(listJoueur[3]);
         }
     }
+    jUn.value = "";
+    jDeux.value = "";
+    jTrois.value = "";
+    jQuatre.value = "";
 });
