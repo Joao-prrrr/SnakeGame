8b503b4ce64a9bd8f517a49da4d2348b05aa8f03
bastien-schnd <batien.schnd@eduge.ch>
2022-10-03 10:53:49 +0200

    affichageQuestion

--- js/question.js
+++ js/question.js
@@ -1,16 +1,191 @@
-const btnValider = document.getElementById("valider");
+const btnValider1 = document.getElementById("valider1");
+const btnValider2 = document.getElementById("valider2");
+const btnValider3 = document.getElementById("valider3");
+const btnValider4 = document.getElementById("valider4");
+const btnValider5 = document.getElementById("valider5");
+const btnValider6 = document.getElementById("valider6");
+const btnValider7 = document.getElementById("valider7");
+const btnValider8 = document.getElementById("valider8");
+const btnValider9 = document.getElementById("valider9");
+const btnValider10 = document.getElementById("valider10");
+const btnValider11 = document.getElementById("valider11");
+const btnValider12 = document.getElementById("valider12");
 const question = document.getElementById("question");
 const plan = document.getElementById("plan");
-const reponse = document.getElementById("correct").checked;
-const nomJoueur = 
+const reponse1 = document.getElementById("correct1").checked;
+const reponse2 = document.getElementById("correct2").checked;
+const reponse3 = document.getElementById("correct3").checked;
+const reponse4 = document.getElementById("correct4").checked;
+const reponse5 = document.getElementById("correct5").checked;
+const reponse6 = document.getElementById("correct6").checked;
+const reponse7 = document.getElementById("correct7").checked;
+const reponse8 = document.getElementById("correct8").checked;
+const reponse9 = document.getElementById("correct9").checked;
+const reponse10 = document.getElementById("correct10").checked;
+const reponse11 = document.getElementById("correct11").checked;
+const reponse12 = document.getElementById("correct12").checked;
 
-btnValider.addEventListener("click", function(){
+btnValider1.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider2.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider3.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider4.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider5.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider6.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider7.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider8.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider9.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider10.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider11.addEventListener("click", function(){
+    question.style.display = "none";
+    plan.style.display = "flex";
+});
+
+btnValider12.addEventListener("click", function(){
     question.style.display = "none";
     plan.style.display = "flex";
 });
 
 function verifReponse(){
-    if (reponse == true){
+    if (reponse1 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse2 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse3 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse4 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse5 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse6 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse7 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse8 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse9 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse10 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse11 == true){
+
+    }
+    else{
+
+    }
+}
+
+function verifReponse(){
+    if (reponse12 == true){
 
     }
     else{
