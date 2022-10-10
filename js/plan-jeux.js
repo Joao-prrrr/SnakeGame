8b1c5343b3f5418faf4a6129cc7435b208716cfb
Louis-Saulnier <louis.slnr@eduge.ch>
2022-10-10 10:49:51 +0200

    merging

--- js/plan-jeux.js
+++ js/plan-jeux.js
@@ -6,6 +6,50 @@ const btnStart = document.getElementById('btnStart')
 let listPion = []
 
 
+<<<<<<< HEAD
+let pionRougeObj = new Pion({
+    nomJoueur: listJoueur[0],
+    id: document.getElementById('pionRouge'),
+    enJeu: true,
+    case: 0
+})
+
+let pionBleuObj = new Pion({
+    nomJoueur: listJoueur[1],
+    id: document.getElementById('pionBleu'),
+    enJeu: false,
+    case: 0
+})
+
+if (listJoueur.length >= 2) {
+    let pionVertObj = new Pion({
+        nomJoueur: listJoueur[2],
+        id: document.getElementById('pionVert'),
+        enJeu: false,
+        case: 0
+    })
+    if (listJoueur.length == 3) {
+        let pionOrangeObj = new Pion({
+            nomJoueur: listJoueur[3],
+            id: document.getElementById('pionOrange'),
+            enJeu: false,
+            case: 0
+        })
+    }
+
+}
+
+listPions = [pionRougeObj, pionBleuObj]
+
+try {
+    listPions.push(pionVertObj)
+} catch { console.log('vert exite pas') }
+
+try {
+    listPions.push(pionOrangeObj)
+} catch { console.log('Orange exite pas') }
+=======
+>>>>>>> 5783d43caae4eb2118687351dd1e51640783188c
 
 btnDe.addEventListener('click', async function() {
     nbAleatoire = await lanceDe()
@@ -19,24 +63,24 @@ btnStart.addEventListener('click', () => {
         enJeu: true,
         case: 0
     })
-
+    
     let pionBleuObj = new Pion({
         nomJoueur: listJoueur[1],
         id: document.getElementById('pionBleu'),
         enJeu: false,
         case: 0
     })
-
+    
     listPion = [pionRougeObj, pionBleuObj]
-
-    if (listJoueur.length > 2) {
+    
+    if(listJoueur.length > 2){
         let pionVertObj = new Pion({
             nomJoueur: listJoueur[2],
             id: document.getElementById('pionVert'),
             enJeu: false,
             case: 0
         })
-        if (listJoueur.length == 3) {
+        if(listJoueur.length == 3){
             let pionOrangeObj = new Pion({
                 nomJoueur: listJoueur[3],
                 id: document.getElementById('pionOrange'),
@@ -44,24 +88,29 @@ btnStart.addEventListener('click', () => {
                 case: 0
             })
         }
-
+    
     }
-
+    
     try {
         listPion.push(pionVertObj)
-    } catch { console.log('vert exite pas') }
-
+    } catch {console.log('vert exite pas')}
+    
     try {
         listPion.push(pionOrangeObj)
-    } catch { console.log('Orange exite pas') }
+    } catch {console.log('Orange exite pas')}
     debuterPartie()
 })
 
 function debuterPartie() {
     partieEnJeu = true;
 
-    for (let i = 0; i < listPion.length; i++) {
+<<<<<<< HEAD
+    for (let i = 0; i < listPions.length; i++) {
+        let pion = listPions[i];
+=======
+    for(let i = 0; i < listPion.length; i++) {
         let pion = listPion[i];
+>>>>>>> 5783d43caae4eb2118687351dd1e51640783188c
         console.log(i)
         pion.mettrePionEnPlace()
         pion.data.id.style.top = "65%"
@@ -74,22 +123,35 @@ function debuterPartie() {
     //     pion.data.id.style.left = "8%"
     // })
 
+<<<<<<< HEAD
+    while (partieEnJeu) {
+
+        listPions.forEach(function(pion) {
+            if (pion.data.enJeu) {
+                pion.avancer(nbAleatoire)
+                pion.data.enJeu = false
+            }
+        })
+
+    }
+=======
     // while(partieEnJeu) {
 
-
-
+        
+    
     // }
+>>>>>>> 5783d43caae4eb2118687351dd1e51640783188c
 
 }
 
 function jouerTour() {
     let prochainJoueur = "";
-    for (let i = 0; i < listPion.length; i++) {
+    for(let i = 0; i < listPion.length; i++) {
         let joueur = listPion[i]
-        if (joueur.data.enJeu) {
+        if(joueur.data.enJeu) {
             joueur.avancer(nbAleatoire)
             joueur.data.enJeu = false
-            if ((listPion.findIndex(joueur => joueur) + 1) > listPion.length) {
+            if((listPion.findIndex(joueur => joueur) + 1) > listPion.length) {
                 prochainJoueur = 0;
             } else {
                 prochainJoueur = listPion[listPion.findIndex(joueur => joueur) + 1]
